4499f9c1a72de10ee20bafb0f7235d395dfec76d
Sanghwa Min <secured8372@gmail.com>
2022-04-12 14:51:06 +0900

    top/bottom fragment update

--- app/src/main/java/com/example/bamsaemkorean/HomeActivity.java
+++ app/src/main/java/com/example/bamsaemkorean/HomeActivity.java
@@ -1,24 +1,14 @@
 package com.example.bamsaemkorean;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
-import android.media.Image;
 import android.os.Bundle;
-import android.view.Gravity;
-import android.view.Menu;
-import android.view.View;
-import android.widget.ImageButton;
 import android.widget.TableRow;
 
 public class HomeActivity extends AppCompatActivity {
 
-    DrawerLayout drawerLayout;
-    View drawerView;
     TableRow category_study_button;
-    ImageButton drawerButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,18 +16,19 @@ public class HomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_home);
 
         category_study_button = (TableRow) findViewById(R.id.category_study_button);
-        drawerButton = (ImageButton) findViewById(R.id.imageButton2);
 
         category_study_button.setOnClickListener(view -> {
             Intent intent = new Intent(getApplicationContext(), CategoryActivity.class);
             startActivity(intent);
         });
 
+        /*
         drawerButton.setOnClickListener(view -> {
             drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
             drawerView = (View) findViewById(R.id.drawer_view);
             if (!drawerLayout.isDrawerOpen(Gravity.LEFT))
                 drawerLayout.openDrawer(Gravity.LEFT);
         });
+        */
     }
 }
