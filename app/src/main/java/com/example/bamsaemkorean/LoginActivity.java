f0135ac41a510ed60479c0ad5b416009211c55a0
DESKTOP-IFKVEA0\LeeMyungMo <lukei321@kakao.com>
2022-05-06 14:49:15 +0900

    UsingIntent_0430

--- app/src/main/java/com/example/bamsaemkorean/LoginActivity.java
+++ app/src/main/java/com/example/bamsaemkorean/LoginActivity.java
@@ -2,23 +2,28 @@ package com.example.bamsaemkorean;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.res.Resources;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
-
     EditText id_text;
     EditText pw_text;
     Button ok_button;
     Button cancel_button;
+    Spinner spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +34,7 @@ public class LoginActivity extends AppCompatActivity {
         pw_text = (EditText) findViewById(R.id.pw_text);
         ok_button = (Button) findViewById(R.id.ok_button);
         cancel_button = (Button) findViewById(R.id.cancel_button);
+        spinner = (Spinner) findViewById(R.id.language_choice);
 
         ok_button.setOnClickListener(v -> {
             if (id_text.getText().toString().equals("test") && pw_text.getText().toString().equals("test")) {
@@ -40,8 +46,32 @@ public class LoginActivity extends AppCompatActivity {
                 Toast.makeText(getApplicationContext(), "잘못된 로그인 정보입니다.",Toast.LENGTH_SHORT).show();
             }
         });
+
         cancel_button.setOnClickListener(v -> {
             System.out.println("cancel clicked");
+        });
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
+                if(pos == 0){
+                    ok_button.setText("ko");
+                }
+                else if(pos == 1){
+                    ok_button.setText("en");
+                }
+                else if(pos == 2){
+                    ok_button.setText("ch");
+                }
+                else{
+                    ok_button.setText("jp");
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
         });
     }
 
